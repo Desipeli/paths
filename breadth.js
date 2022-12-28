f845e79c1ae36e187cfb632a31e05b5b2dcb60bd
Desipeli <mr.desipeli@gmail.com>
2022-12-28 19:46:02 +0200

    shortest path

--- breadth.js
+++ breadth.js
@@ -1,24 +1,28 @@
 import NodeValues from "./resources.js"
-import { map, fillMapWith, randomElementFromArray, randomInt, delay, animDelay} from "./resources.js"
+import { map, replaceCellsWith, delay, animDelay} from "./resources.js"
 import { drawCell, drawGrid, drawMap } from "./canvas.js"
 import HeapQ from "./heapq.js"
 
 
-const initBreadth = () => {
+const initBreadth = async (startPos, endPos) => {
     const condition = (bigger, smaller) => (bigger.dist > smaller.dist)
     const queue = new HeapQ(condition)
     const visited = {}
-    const startPos = {dist: 0, x: 1, y: 1}
-    const endPos = {dist: Infinity, x: map.length -2, y: map.length -2}
+    startPos = {dist: 0, x: startPos.x, y: startPos.y}
+    endPos = {dist: Infinity, x: endPos.x, y: endPos.y}
     const adjList = createAdjList()
     queue.push(startPos)
     const distances = setDistances()
     distances[startPos.y][startPos.x] = 0
-    breadth(queue, visited, adjList, distances)
+    const previous = {}
+    await breadth(queue, visited, adjList, distances, previous)
+    const path = getShortestPath(previous, startPos, endPos)
+    showShortestPath(path)
 }
 
-const breadth = async (queue, visited, adjList, distances) => {
+const breadth = async (queue, visited, adjList, distances, previous) => {
     let safety = 0
+    let endFound = false
     
     while (!queue.isEmpty()) {
         const newNode = queue.pop()
@@ -28,16 +32,22 @@ const breadth = async (queue, visited, adjList, distances) => {
         if (map[newNode.y][newNode.x] == NodeValues.EMPTY) {
             map[newNode.y][newNode.x] = NodeValues.VISITED
         }
-        await delay(animDelay)
+        
         drawCell({x: newNode.x, y: newNode.y})
         adjList[[newNode.y, newNode.x]].forEach(edge => {
-            const newDist = newNode.dist + 1
+            const newDist = distances[newNode.y][newNode.x] + 1
             if (newDist < distances[edge.y][edge.x]) {
+                previous[[edge.y, edge.x]] = newNode
                 distances[edge.y][edge.x] = newDist
+                if (map[edge.y][edge.x] == NodeValues.END) {
+                    endFound = true
+                }
                 queue.push({dist: newDist, x: edge.x, y: edge.y})
             }
         })
-        if (safety >= 10000) return
+        if (animDelay > 0) await delay(animDelay)
+        if (endFound) break
+        if (safety >= 100000) return
         safety++
     }
 }
@@ -51,6 +61,32 @@ const setDistances = () => {
         }
     }
     return dists
+}
+
+const showShortestPath = (path) => {
+    replaceCellsWith(NodeValues.VISITED, NodeValues.EMPTY)
+    drawGrid()
+    drawMap()
+    for (let i = 0; i < path.length; i++) {
+        map[path[i].y][path[i].x] = NodeValues.ROUTE
+        console.log(path[i].x, path[i].y)
+        drawCell({x: path[i].x, y: path[i].y})
+    }
+}
+
+const getShortestPath = (previous, startPos, endPos) => {
+    const path = []
+    let prev = endPos
+    let safety = 100000
+    while (true) {
+        prev = previous[[prev.y, prev.x]]
+        if (!prev) break
+        path.push({y: prev.y, x: prev.x})
+        safety --
+        if (safety == 0) break
+    }
+    path.pop() // pop start
+    return path.reverse()
 }
 
 const createAdjList = () => {
